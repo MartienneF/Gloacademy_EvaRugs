2c932d9903aea3bb30c2c95e6ad2a52349f8ce8d
MartienneF <93133566+MartienneF@users.noreply.github.com>
2022-01-28 17:16:12 +0200

    День 4. Работаем с модальным окном

--- js/main.js
+++ js/main.js
@@ -6,15 +6,36 @@ const swiper = new Swiper('.swiper', {
   centeredSlides: true,
   slideToClickedSlide: true,
   toggle: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
   breakpoints: {
     320: {
       slidesPerView: 1
     },
     768: {
-      slidesPerView: 1
+      slidesPerView: 2
     },
     1440: {
       slidesPerView: 3
     }
+  }
+});
+
+const windowModal = document.querySelector('.modal');
+const body = document.querySelector('body');
+const buttonModal = document.querySelector('.main-display__button');
+
+buttonModal.addEventListener('click', () => {
+  windowModal.classList.add('active');
+  body.classList.add('lock');
+});
+
+windowModal.addEventListener('click', (e) => {
+  const isModal = e.target.closest('.modal__inner');
+  if (!isModal) {
+    windowModal.classList.remove('active');
+    body.classList.remove('lock');
   }
 });
